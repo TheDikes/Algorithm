dc954cb956e36afc48bdf7721b22945e9ca8416e
Cynthia Dike <Maa-mmy>
2024-02-07 20:41:54 +0100

    365 days Algorithm challenge ...

--- replace end.py
+++ replace end.py
@@ -6,7 +6,7 @@ def replace_ending(sentence, old, new):
         # end with the new string
         i = len(sentence) - len(old)
         
-        new_sentence = sentence[:-len(old)] + new
+        new_sentence = sentence[:i] + new
         return new_sentence
 
 
